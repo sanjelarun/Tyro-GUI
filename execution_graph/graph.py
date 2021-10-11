6cc1ed4235030e4333a21998ac144711c9e3446b
sanjelarun <sanjelarun@gmail.com>
2021-10-11 13:57:20 -0500

    Created Structure for graph and nodes

---
+++ execution_graph/graph.py
@@ -0,0 +1,9 @@
+class Graph:
+    def __init__(self):
+        self.vertices = []
+    def hasCylces(self):
+        return false
+    def getCycleNodes(self):
+        return null
+
+
---
+++ execution_graph/node.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self):
+        self.id = ""
+        self.ast_node = ""
+        self.code_in_string= ""
+        self.next = []
+
